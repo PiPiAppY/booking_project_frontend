758fa73cafcb70b45f63ad79b2ae3c9a0a8dff0b
unknown <titov6180@gmail.com>
2026-04-17 19:14:03 +0300

    fhf2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import Login from './components/Login';
 import Register from './components/Register';
